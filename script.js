8ad3679eb376a1d42c4911350dc41b5479fff633
Luciano Torrano <lucianotorrano.dev@gmail.com>
2022-04-04 23:11:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/***** BOTON DE MENU RESPONSIVE ******/
+
+const menuRespBtn = document.querySelector('.menuResponsive-btn');
+
+menuRespBtn.onclick = ()=>{
+	menuRespBtn.classList.toggle('active');
+}
+
